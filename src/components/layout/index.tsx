531f383eb85fd19f330a08795ed83dc5156627df
unknown <phucp9698@gmail.com>
2026-08-17 16:41:19 +0700

    UI: adding footer

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,16 @@
+
+import Header from '../../header'
+import Footer from '../../footer'
+import { Outlet } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <div>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Layout
